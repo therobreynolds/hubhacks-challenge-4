e6b3fa7e7f7c9d821834e839cd491415ffcc96ee
therobreynolds <rreynolds@qlarion.com>
2014-08-10 12:45:04 -0400

    Update details.js

--- js/details.js
+++ js/details.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
 	var id = location.search.replace('?', '').split('=')[1];
 	var appData;
-	var reviewsData = "";
+	// var reviewsData = "";
 	var reviewProgress = "";
+	var defaultDuration = "";
 
 	// only if an ID is given
+	//alert(id);
 	if(id) {
 		// get overall info
 		$.ajax({
@@ -16,47 +18,50 @@ $(document).ready(function() {
 				appData = data;
 				$('.container > p').html("Hello, <i>"+ data.Applicants[0].Contact.Identity.LastName + "</i>!<br> Your application status is: ");
 				$('#statusLbl').text(data.ProcessState.Code);
-				//$('#reviews').text(data.Reviews.0.ReviewType.Description);
+
+				//get milestone data based on the returned result of the initial REST query
+				$.ajax({
+					//url: "http://rawgit.com/agraebe/hubhacks-challenge-4/master/json/milestones.json",
+					url: "json/milestones.json",
+					type: "GET",
+					dataType: "json",
+					success: function (milestones) {
+						$.each( milestones, function( key, value ) {
+							if(value.Milestone === appData.ProcessState.Code) {
+							  	console.log(value);
+							  	$('#statusLbl').text(value.Status);
+							  	$('#descriptionLbl').text(value.Description);
+							  	if(value.Duration === "" || value.Duration === " "){
+							  		defaultDuration = "Duration unavailable";
+							  		$('#durationText').text(defaultDuration);
+							  	}
+							  	else {
+							  		$('#durationText').text(value.Duration);
+							  	}
+							  	$('#performingText').text(value.WhoPerforming);
+							  	$('#contactText').text(value.Contact);
+							}
+						});
+					}
+				});
 			},
 			failure: function (xqr) {
-				alert("Failed to load Process State data!");
+				alert("Failed to load data!");
 			}
 		});
+		//getMilestonesFunction(appData);
+		getReviewsFunction(id);
 
-		// get additional staff info
-		/*
-		$.ajax({
-			url: "http://rawgit.com/agraebe/hubhacks-challenge-4/master/json/staff.json",
-			type: "GET",
-			dataType: "json",
-			success: function (staff) {
-				console.log(staff);
-			}
-		});
-		*/
 
-		//get additional milestone info
-		$.ajax({
-			url: "http://rawgit.com/agraebe/hubhacks-challenge-4/master/json/milestones.json",
-			type: "GET",
-			dataType: "json",
-			success: function (milestones) {
-				$.each( milestones, function( key, value ) {
-				  if(value.Milestone === appData.ProcessState.Code) {
-				  	console.log(value);
-				  	$('#statusLbl').text(value.Status);
-				  	$('#descriptionLbl').text(value.Description);
-				  	$('#durationText').text(value.Duration);
-				  	$('#performingText').text(value.WhoPerforming);
-				  	$('#contactText').text(value.Contact);
-				  }
-				});
-			}
-		});
 
-		//get Review info
+	}
+})
+
+function getReviewsFunction(permitID){
+		var reviewsData = "";
+			//get Review info
 		$.ajax({
-			url: "https://permitapidev.cityofboston.gov:4443/api/building/applicationinfo/"+id,
+			url: "https://permitapidev.cityofboston.gov:4443/api/building/applicationinfo/"+permitID,
 			type: "GET",
 			dataType: "json",
 			success: function (reviews) {
@@ -86,6 +91,4 @@ $(document).ready(function() {
 				$('#reviewsText').html(reviewsData);
 			}
 		});
-
-	}
-})
+}
