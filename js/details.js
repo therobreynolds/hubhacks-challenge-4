75b4c652d4fb905bd648bdc02e0ff20496829478
Graebe, Alexander <alex.graebe@gmail.com>
2014-08-10 02:58:59 -0400

    Added Status Boxes

--- js/details.js
+++ js/details.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 	var id = location.search.replace('?', '').split('=')[1];
 	var appData;
+	var reviewsData = "";
+	var reviewProgress = "";
 
 	// only if an ID is given
 	if(id) {
@@ -14,9 +16,10 @@ $(document).ready(function() {
 				appData = data;
 				$('.container > p').html("Hello, <i>"+ data.Applicants[0].Contact.Identity.LastName + "</i>!<br> Your application status is: ");
 				$('#statusLbl').text(data.ProcessState.Code);
+				//$('#reviews').text(data.Reviews.0.ReviewType.Description);
 			},
 			failure: function (xqr) {
-				alert("Failed to load data!");
+				alert("Failed to load Process State data!");
 			}
 		});
 
@@ -43,8 +46,44 @@ $(document).ready(function() {
 				  	console.log(value);
 				  	$('#statusLbl').text(value.Status);
 				  	$('#descriptionLbl').text(value.Description);
+				  	$('#durationText').text(value.Duration);
+				  	$('#performingText').text(value.WhoPerforming);
+				  	$('#contactText').text(value.Contact);
 				  }
 				});
+			}
+		});
+
+		//get Review info
+		$.ajax({
+			url: "https://permitapidev.cityofboston.gov:4443/api/building/applicationinfo/"+id,
+			type: "GET",
+			dataType: "json",
+			success: function (reviews) {
+				console.log(reviews);
+				//console.log(reviewsData);
+				$.each( reviews.Reviews, function( key, value ) {
+				   if(value.ReviewResult.Code === " ") {
+				   		reviewProgress = "Not yet Assigned";
+				   		if(value.IsAssigned === "Y"){
+				   			reviewProgress = "Assigned, but not Started";
+				   		}
+				   		if(value.IsStarted === "Y"){
+				   			reviewProgress = "Started, but not yet Complete";
+				   		}
+				   		if(value.IsComplete === "Y"){
+				   			reviewProgress = "Complete, and awaiting Result";
+				   		}
+				   }
+				   else {
+				   		reviewProgress = value.ReviewResult.Code;
+				   }
+				   	console.log(value);
+				  	reviewsData += value.ReviewType.Description + " : " +reviewProgress + "<br>"; //= value.ReviewResult.Code;
+				  	
+				  	
+				});
+				$('#reviewsText').html(reviewsData);
 			}
 		});
 
